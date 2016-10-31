debbd519e3a4122590b8e2a51dea43a54381081a
Karan <karandeepbatra@gmail.com>
2016-10-31 16:14:47 +0530

    Add Trie TopCoder Tutorial

---
+++ Trie/trie.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define pb push_back
+
+const int MAXN = 26;
+
+struct Trie{
+	int words;
+	int prefixes;
+	Trie* edges[MAXN];
+	Trie(){
+		words = 0;
+		prefixes = 0;
+		for(int i = 0 ;i < MAXN; i++)
+			edges[i] = NULL;
+	}
+};
+
+Trie root;
+int size;
+string word;
+
+void addWord(Trie* vertex, int idx){
+	if(idx == size){
+		vertex->words++;
+		return;
+	}
+	vertex->prefixes++;
+	if(vertex->edges[word[idx]-'a'] == NULL){
+		vertex->edges[word[idx]-'a'] = new Trie;
+	}
+	addWord(vertex->edges[word[idx]-'a'], idx+1);
+}
+
+int countWords(Trie* vertex, int idx){
+	if(idx == size)
+		return vertex->words;
+	else if(vertex->edges[word[idx]-'a'] == NULL)
+		return 0;
+	return countWords(vertex->edges[word[idx]-'a'], idx+1);
+}
+
+int countPrefixes(Trie* vertex, int idx){
+	if(idx == size)
+		return vertex->prefixes;
+	else if(vertex->edges[prefix[idx]-'a'] == NULL)
+		return 0;
+	return countPrefixes(vertex->edges[prefix[idx]-'a'], idx+1);
+}
+
+
+int main(){
+	cout << root.words << endl;
+	return 0;
+}
