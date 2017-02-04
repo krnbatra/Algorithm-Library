c1f2f9bf55127bfc3e134612e7378520e7c2dcdd
Karan <karandeepbatra@gmail.com>
2017-02-04 22:54:55 +0530

    Update graph templates with smaller ones

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,28 +1,19 @@
 /*My First Template :D*/
 #include <bits/stdc++.h>
-#include <limits.h>
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef vector<int> vi;
  
 #define MOD (ll)1000000007
 #define pb   push_back
 #define EPS 1e-9
 #define FOR(i,n)  for(int i = 0;i < n; i++)
 #define FORE(i,a,b) for(int i = a;i <= b; i++)
-#define pi(a)   printf("%d\n", a)
-#define all(c)  c.begin(), c.end()
 #define tr(container, it)   for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
-#define gc getchar_unlocked
-#define sdi(a, b)   si(a);si(b)
 #define io ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define endl '\n'
 #define F first
 #define S second
-#define FILL(arr, val)  memset(arr, val, sizeof(arr))
-#define read(arr, n)    for(int i = 0;i < n; i++)cin>>arr[i];
 #define sp ' '
 
 template <typename T> T gcd(T a, T b){return (b==0)?a:gcd(b,a%b);}
@@ -31,16 +22,6 @@ template <typename T> T mod_exp(T b, T p, T m){T x = 1;while(p){if(p&1)x=(x*b)%m
 template <typename T> T invFermat(T a, T p){return mod_exp(a, p-2, p);}
 template <typename T> T exp(T b, T p){T x = 1;while(p){if(p&1)x=(x*b);b=(b*b);p=p>>1;}return x;}
 
-void si(int &x){
-    register int c = gc();
-    x = 0;
-    int neg = 0;
-    for(;((c<48 || c>57) && c != '-');c = gc());
-    if(c=='-') {neg=1;c=gc();}
-    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
-    if(neg) x=-x;
-}
-
 const int MAXN = 1e5+5;
 // int INF = 1e6;
 vector<ii> adj[MAXN];   //{dist, adjacent_vertex}
@@ -48,7 +29,7 @@ ll dist[MAXN];
 bool vis[MAXN];
 int n, m, st, finish;
 
-void initalize(){
+void init(){
     FORE(i,1,n){adj[i].clear();dist[i]=1e6;vis[i]=false;}
 }
 
@@ -81,7 +62,7 @@ int main(){
     cin >> t;
     while(t--){
         cin >> n >> m >> st >> finish;
-        initalize();
+        init();
         FOR(i,m){
             int a, b, w;
             cin >> a >> b >> w;
