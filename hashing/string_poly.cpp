48d1b90739955c0fbbefd6314bd5b33862d07b52
Karan <karandeepbatra@gmail.com>
2017-09-06 02:34:05 +0530

    Updates

--- hashing/string_poly.cpp
+++ hashing/string_poly.cpp
@@ -1,4 +1,4 @@
-/*My First Template :D*/
+/*Let's get high :D*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -15,63 +15,59 @@ template <typename T> T invFermat(T a, T p){return mod_exp(a, p-2, p);}
 template <typename T> T exp(T b, T p){T x = 1;while(p){if(p&1)x=(x*b);b=(b*b);p=p>>1;}return x;}
 
 const int MAXN = 1e5+5;
-
+string str;
+int n;
 ll MOD1 = MOD;
-ll MOD2 = MOD+2;
 ll p1 = 43LL;
-ll p2 = 107LL;
-ll power1[MAXN], power2[MAXN];
+ll power1[MAXN];
 ll prefix[MAXN], suffix[MAXN];
 
 void init(){
-	power1[0] = p1;
-	power2[0] = p2;
-	for(int i = 1;i <= (int)3e5+5; i++){
-		power1[i] = (power1[i-1]*p1) % MOD;
-		power2[i] = (power2[i-1]*p2) % MOD2; 
-	}
+    power1[0] = 1;
+    for(int i = 1;i <= 100000; i++)
+        power1[i] = (power1[i-1] * p1) % MOD;
 }
 
 void calPrefix(){
-	prefix[0] = str[0];
-	for(int i = 1; i < str.size(); i++)
-		prefix[i] = (prefix[i-1] + (str[i]*mod_exp(26LL, (ll)i, MOD)) % MOD) % MOD;
+    prefix[0] = str[0] % MOD;
+    for(int i = 1; i < str.size(); i++)
+        prefix[i] = (prefix[i-1] + (str[i] * power1[i]) % MOD) % MOD;
 }
 
 void calSuffix(){
-	suffix[0] = str[n-1];
-	int j = 1;
-	
-	for(int i = n-2;i >= 0; i--){
-		suffix[j] = (suffix[j-1] + (str[i]*mod_exp(26LL, (ll)j, MOD)) % MOD) % MOD;
-		j++;
-	}
+    suffix[0] = str[n-1] % MOD;
+    int j = 1;
+    
+    for(int i = n-2;i >= 0; i--){
+        suffix[j] = (suffix[j-1] + (str[i] * power1[j]) % MOD) % MOD;
+        j++;
+    }
 }
 
 int main(){
     io;
     init();
-	string str = "bab";
-	int n = str.size();
-	
-	int l, r;
-	cin >> l >> r;
-	
-	calPrefix();
-	calSuffix();
+    str = "bab";
+    n = str.size();
+    
+    int l, r;
+    cin >> l >> r;
+    
+    calPrefix();
+    calSuffix();
 
-	ll hash;
-	if(l > 0)
-		hash = ((prefix[r]-prefix[l-1])*invFermat(mod_exp(26LL, (ll)l, MOD), MOD)) % MOD;
-	else
-		hash = prefix[r];
-	
-	ll revHash;
-	if(r > 0)
-		revHash = ((suffix[n-l-1]-suffix[n-r-2])*invFermat(mod_exp(26LL, (ll)n-r-1, MOD), MOD)) % MOD;
-	else
-		revHash = suffix[0];
-	
-	cout << hash << ' ' << revHash << endl;
+    ll hash;
+    if(l > 0)
+        hash = ((prefix[r] - prefix[l-1]) * invFermat(power1[l], MOD)) % MOD;
+    else
+        hash = prefix[r];
+    
+    ll revHash;
+    if(r > 0)
+        revHash = ((suffix[n-l-1] - suffix[n-r-2]) * invFermat(power1[n-r-1], MOD)) % MOD;
+    else
+        revHash = suffix[0];
+    
+    cout << hash << ' ' << revHash << endl;
     return 0;
 }
--- number_theory/nCr.cpp
+++ number_theory/nCr.cpp
@@ -15,14 +15,25 @@ template <typename T> T invFermat(T a, T p){return mod_exp(a, p-2, p);}
 template <typename T> T exp(T b, T p){T x = 1;while(p){if(p&1)x=(x*b);b=(b*b);p=p>>1;}return x;}
 
 const int MAXN = 1e5+5;
+ll fact[MAXN], invp[MAXN];
+
+void init(){
+    fact[0] = 1, invp[0] = 1;
+    for(int i = 1; i < MAXN; i++)
+        fact[i] = (fact[i-1] * i) % MOD;
+    invp[MAXN-1] = invFermat(fact[MAXN-1], MOD);
+    for(int i = MAXN-2; i >= 1; i--)
+        invp[i] = (invp[i+1] * (i+1)) % MOD;
+}
 
 ll nCr(int n, int r){
-	if(r == 0)	return 1;
-	return nCr(n-1, r-1) * n/r;
+    if(r < 0 || r > n)  return 0;
+    return (fact[n] * ((invp[n-r] * invp[r]) % MOD)) % MOD;
 }
 
 int main(){
     io;
+    init();
     cout << nCr(5, 3) << endl;
     return 0;
 }
