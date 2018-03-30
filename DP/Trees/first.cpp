c5a3170a1cefae8ffbb6a414ceb94766c7dd5a02
Karan <karandeepbatra@gmail.com>
2018-03-30 22:43:30 +0530

    Add TRACE in template

--- DP/Trees/first.cpp
+++ DP/Trees/first.cpp
@@ -1,4 +1,3 @@
-/*Let's get high :D*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -6,15 +5,8 @@ typedef long long ll;
 #define MOD                 1000000007LL
 #define EPS                 1e-9
 #define io                  ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define M_PI                3.14159265358979323846
 
-template <typename T> T gcd(T a, T b){return (b==0)?a:gcd(b,a%b);}
-template <typename T> T lcm(T a, T b){return a*(b/gcd(a,b));}
-template <typename T> T mod_exp(T b, T p, T m){T x = 1;while(p){if(p&1)x=(x*b)%m;b=(b*b)%m;p=p>>1;}return x;}
-template <typename T> T invFermat(T a, T p){return mod_exp(a, p-2, p);}
-template <typename T> T exp(T b, T p){T x = 1;while(p){if(p&1)x=(x*b);b=(b*b);p=p>>1;}return x;}
-
-const int MAXN = 1e5+5;
+const int MAXN = 1e5 + 5;
 vector<int> adj[MAXN];
 int dp1[MAXN], dp2[MAXN];
 
@@ -26,16 +18,15 @@ void dfs(int u, int parent){
         sum1 += dp2[v];
         sum2 += max(dp1[v], dp2[v]);
     }
-    dp1[u] += cost[u];
+    dp1[u] = cost[u] + sum1;
     dp2[u] = sum2;
-
 }
 
 int main(){
     io;
     int n;
     cin >> n;
-    for(int i = 1;i < n; i++){
+    for(int i = 1; i < n; i++){
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
--- template_new.cpp
+++ template_new.cpp
@@ -2,14 +2,24 @@
 using namespace std;
 typedef long long ll;
  
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+
 #define MOD                 1000000007LL
 #define EPS                 1e-9
 #define io                  ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define endl				'\n'
-#define trace1(x)			cerr<<#x<<": "<<x<<endl
-#define trace2(x, y)		cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
-#define trace3(x, y, z)		cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
-#define trace4(a, b, c, d)	cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
 
 const int N = 1e5 + 5;
 
